293ea4e7591e6b00a313e9c34d62c6f2bb95471e
Izabela Stawicka <izabela.stawicka11@wp.pl>
2023-04-16 18:50:11 +0200

    End of section 4

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
-import { Button, Header, List } from 'semantic-ui-react';
+import { Header, List } from 'semantic-ui-react';
 
 
 function App() {
