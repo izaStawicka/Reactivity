e9029cad2592aa8fd2392cc940efc804f56828e9
Izabela Stawicka <izabela.stawicka11@wp.pl>
2023-06-27 13:27:50 +0200

    try to change homepage

--- client-app/src/features/activities/home/HomePage.tsx
+++ client-app/src/features/activities/home/HomePage.tsx
@@ -17,7 +17,7 @@ export default observer( function HomePage(){
                 </Header>
                 {userStore.isLoggedIn ? (
                     <>
-                        <Header as='h2' inverted content='Welcome to Reactivities'/>
+                        <Header as='h2' inverted content='Welcome to Reactivities!!!'/>
                         <Button as={Link} to='/activities' content='Go to Activities!'/>
                     </>
                 ) : (
